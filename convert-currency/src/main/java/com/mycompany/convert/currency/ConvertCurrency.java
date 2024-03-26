07fe5e1e4cfe4d297aa37aae8516a3a171d6c474
waelghouma <waelghouma28@gmail.com>
2024-03-26 04:22:59 +0100

    setup

---
+++ convert-currency/src/main/java/com/mycompany/convert/currency/ConvertCurrency.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convert.currency;
+
+/**
+ *
+ * @author waelg
+ */
+public class ConvertCurrency {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
